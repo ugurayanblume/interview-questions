9f3ee28f4b803d919934d9b1cc789d6c508ec34c
Ugur Ayan <ugur.ayan@gmail.com>
2018-02-19 07:09:25 +0100

    update

--- src/com/ugurayan/java/algorithm/C1_2_reverse_c_string.java
+++ src/com/ugurayan/java/algorithm/C1_2_reverse_c_string.java
@@ -1,7 +1,36 @@
 package com.ugurayan.java.algorithm;
 
 /**
- * Created by future on 2/19/18.
+ * Write code to reverse a C-Style String. (C-String means that “abcd” is represented as
+ * five characters, including the null character.)
  */
 public class C1_2_reverse_c_string {
+
+    // C Code
+    /*void reverse(char *str) {
+        char * end = str;
+        char tmp;
+        if (str) {
+            while (*end) {
+                ++end;
+            }
+            --end;
+            while (str < end) {
+                tmp = *str;
+                *str++ = *end;
+                *end-- = tmp;
+            }
+        }
+    }*/
+
+    public String reverse(String str) {
+
+        StringBuilder temp = new StringBuilder(str);
+        int lngth= str.length();
+        for (int i = 0; i < lngth; i++){
+            temp.setCharAt(lngth - i, str.charAt(i));
+        }
+
+        return temp.toString();
+    }
 }
---
+++ src/com/ugurayan/java/algorithm/C1_3_remove_duplicates.java
@@ -0,0 +1,7 @@
+package com.ugurayan.java.algorithm;
+
+/**
+ * Created by future on 2/19/18.
+ */
+public class C1_3_remove_duplicates {
+}
