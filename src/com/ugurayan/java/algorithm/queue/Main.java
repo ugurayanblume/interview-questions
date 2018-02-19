36445f1167a557a5c317654387f54f40e351ec23
Ugur Ayan <ugur.ayan@gmail.com>
2018-02-19 11:37:52 +0100

    queue

--- src/com/ugurayan/java/algorithm/queue/Main.java
+++ src/com/ugurayan/java/algorithm/queue/Main.java
@@ -1,68 +1,18 @@
-package com.ugurayan.java.algorithm.linkedList;
+package com.ugurayan.java.algorithm.queue;
+
+
 
 public class Main {
-
-    static Node first = new Node(10);
+    static Queue first = new Queue();
 
     public static void main(String[] args) {
-        addNode(5);
-        printLinkedList();
 
-        addNode(16);
-        printLinkedList();
+        first.enqueue(5);
+        first.enqueue(7);
+        first.enqueue(9);
+        System.out.println(first.toString());
 
-        addNode(3);
-        addNode(new Node (6));
-        printLinkedList();
-
-        deleteNode( new Node(16));
-        printLinkedList();
-
-    }
-
-    public static void addNode(Node a) {
-        Node head = first;
-
-        while(head.next != null) {
-            head = head.next;
-        }
-        head.next = a;
-    }
-
-    public static void addNode(int a) {
-        Node temp = new Node(a);
-        Node head = first;
-
-        while(head.next != null) {
-            head = head.next;
-        }
-        head.next = temp;
-    }
-
-    public static void deleteNode(Node a) {
-        Node head = first;
-
-        while (head.next  != null){
-            if (head.data == a.data) {
-                head.data = head.next.data;
-                head.next= head.next.next;
-            }
-            head = head.next;
-        }
-    }
-
-    public static void printLinkedList() {
-
-        Node temp = first;
-        int i = 0;
-
-        System.out.print( i + ":(" + temp.data + ")");
-        while (temp.next != null) {
-            ++i;
-            temp = temp.next;
-            System.out.print( " > " + i + ":(" + temp.data + ")");
-        }
-        System.out.println();
-
+        first.dequeue();
+        System.out.println(first.toString());
     }
 }
